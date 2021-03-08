8f849384a83709c062e19308bb8e5d057da93db4
tangbl93 <tangbl93@gmail.com>
2021-03-08 10:38:52 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "whenTapped", targets: ["whenTapped"]),
     ],
     dependencies: [
-        .package(name: "Strongify", url: "https://github.com/krzysztofzablocki/Strongify.git", .branch("master")),
+        .package(name: "Strongify", url: "https://gitee.com/yxspecs/Strongify.git", from: "1.2.1"),
     ],
     targets: [.target(name: "whenTapped", dependencies: ["Strongify"])],
     swiftLanguageVersions: [.v5]
